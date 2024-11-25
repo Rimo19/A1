8535deb59449b0a1f9cb40e03373294a9a58073f
Rimo19 <163086529+Rimo19@users.noreply.github.com>
2024-11-25 12:21:49 +0300

    j
    i

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using sestim;
+{
+  puplec class Program Mine ();
+     Voide string ;{
+     
+    string.Name("RIMASS");
+    int.Age("20");
+    string.Grade("first");
+   
+    }
+     console.DisplayInfo(Name); 
+    console. DisplayInfo(Age); 
+    console. DisplayInfo(Grade);
+   
+}
+
+    
